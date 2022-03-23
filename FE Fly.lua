701c935a7bd040b08e7ed176f766dbd543a8a34e
W0ND585 <85813712+W0ND585@users.noreply.github.com>
2022-03-22 22:19:54 -0500

    Update FE Fly.lua

--- FE Fly.lua
+++ FE Fly.lua
@@ -3,7 +3,7 @@ repeat wait()
 local mouse = game.Players.LocalPlayer:GetMouse()
 repeat wait() until mouse
 local plr = game.Players.LocalPlayer
-local torso = plr.Character.Torso
+local torso = plr.Character.PrimaryPart
 local flying = true
 local deb = true
 local ctrl = {f = 0, b = 0, l = 0, r = 0}
